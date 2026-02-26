15bec966639f9999e407103dd5e2f8dfb4fd8d5b
Chuxia-sys <shiela.jkb@gmail.com>
2026-02-26 15:09:04 +0800

    add capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.budgetnest.app',
+  appName: 'BudgetNest',
+  webDir: 'out'
+};
+
+export default config;
